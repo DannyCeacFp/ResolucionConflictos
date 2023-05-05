b9e3f7940f233356d439cb7d444657ef8c55ce6c
DannyCeacFp <danny.cuenca@alu.ceacfp.com>
2023-05-05 13:40:26 +0200

    Modificación ClasePrueaba. Añadiendo saludo

--- Conflictos/src/ClasePrueba.java
+++ Conflictos/src/ClasePrueba.java
@@ -13,6 +13,8 @@ public class ClasePrueba {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		String saludo="Hello world";
+		System.out.println(saludo);
 	}
 
 }
