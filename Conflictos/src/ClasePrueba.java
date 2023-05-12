0186fa4df0ddfb0d4df79d3600b61d98851fa8cd
DannyCeacFp <danny.cuenca@alu.ceacfp.com>
2023-05-12 13:52:31 +0200

    borrando el contenido

--- Conflictos/src/ClasePrueba.java
+++ Conflictos/src/ClasePrueba.java
@@ -13,11 +13,7 @@ public class ClasePrueba {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		String saludo="Hello world";
-		String salu2="Hola rama master";
-		String saludoRama="Hola rama secundaria";
-		System.out.println(saludo);
-		System.out.println(salu2);
+		//Borrando todo el contenido
 	}
 
 }
